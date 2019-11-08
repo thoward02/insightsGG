c726010ed9996d4a10977696df1545ae1d11f226
thoward02-2ndPeriod <36546676+thoward02@users.noreply.github.com>
2019-11-08 09:43:15 -0500

    Added custom timeline sort

--- Src/Main.py
+++ Src/Main.py
@@ -15,13 +15,37 @@ except:
 #Create instance and login
 App = Insights.App(LoginData["username"], LoginData["password"])
 
+#Login
 print("Logging in... ")
 App.Login()
+print("Logged in. ")
 
+#Run user tasks
+print("Running user tasks...")
 
-x = App.GrabTimeLine(["2HyM0p0pgETw5QlTIJ3Ia"])
+#Loop in users team, grab list of vods, and grab analysis of vods
+TeamName = "RedtailVods"
+VodList = []
+for Vods in App.Teams[TeamName]["VodList"]:
+    print("[" + str(len(VodList)) + "]: " + Vods)
+    VodList.append(Vods)
+
+print("Which one do you wanna fetch the analysis for?")
+
+Analyze = int(input("Vod Num: "))
+
+RawMatchList = App.Teams[TeamName]["VodList"][VodList[Analyze]]["latestAnalysis"]["result"]["matches"]
+IdList   = []
+
+for Matches in RawMatchList:
+    IdList.append(Matches["id"])
+
+#Send of match id list to be read
+
+TimeLine = App.GrabTimeLine(IdList)
 
 with open("Outputs/Output.json", "w") as OutputFile:
-    OutputFile.write(json.dumps(x, indent=4))
+    OutputFile.write(json.dumps(TimeLine, indent=4))
 
-print("done...")
+
+print("Done...")
